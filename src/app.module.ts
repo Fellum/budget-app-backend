8654be7f5b62dbe195f3a4d756c9812353bf5865
Dmitry Golubkov <fkekerman@gmail.com>
2024-05-10 10:32:49 +0400

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
 })
 export class AppModule {}
